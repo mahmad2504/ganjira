ff93439f8d0ae4bee323637c6683305eb3af7bc2
Mumtaz <Mumtaz_Ahmad@mentor.com>
2017-05-09 16:29:23 +0500

    sss

---
+++ projects/wd4g/settings.php
@@ -0,0 +1,18 @@
+<?php 
+
+define('JIRA_URL', 'http://jira.alm.mentorg.com:8080');
+define('JIRA_UPDATE_ALLOWED','false');
+define('QUERY','(project=NUCDM) and labels in (GWD4G)');
+define('USERS_TIMESHEET','');
+define('USERS_WEEKLY_REPORT','');
+define('JIRA_STRUCTURE',637);
+define('PROJECT_LAYOUT',JIRA_STRUCTURE);
+//define('PROJECT_LAYOUT',GAN_FILE);
+
+
+define('TITLE','Wall Demo 4G');
+
+$BOARD1 ='NUCDM-58,NUCDM-56,NUCDM-57';
+$DEFAULT_BOARD = $BOARD1;
+
+?>
---
+++ projects/wfdemo/settings.php
@@ -0,0 +1,18 @@
+<?php 
+
+define('JIRA_URL', 'http://jira.alm.mentorg.com:8080');
+define('JIRA_UPDATE_ALLOWED','false');
+define('QUERY','(project=NUCDM) and labels in (WEATHER_FORD)');
+define('USERS_TIMESHEET','');
+define('USERS_WEEKLY_REPORT','');
+define('JIRA_STRUCTURE',639);
+define('PROJECT_LAYOUT',JIRA_STRUCTURE);
+//define('PROJECT_LAYOUT',GAN_FILE);
+
+
+define('TITLE','Weather Ford');
+
+$BOARD1 ='NUCDM-63,NUCDM-62,NUCDM-76,NUCDM-85,NUCDM-88,NUCDM-91';
+$DEFAULT_BOARD = $BOARD1;
+
+?>
